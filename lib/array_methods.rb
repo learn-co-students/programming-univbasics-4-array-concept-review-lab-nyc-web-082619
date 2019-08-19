830db14b8879293480c58d9c08bcfbe3aa2692f6
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-19 18:02:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,31 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+
+
+def find_element_index(array, value_to_find) 
+array.length.times do |count| 
+    if array[count] == value_to_find
+    return count 
+  end 
+end 
+  nil 
+end 
+
 
 def find_max_value(array)
-  # Add your solution here
+  x= 0 
+array.length.times do |count| 
+  if array[count] > x
+    x = array[count]
+end 
+end 
+  return x 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  x= array[0]
+  array.length.times do |count| 
+    if array[count] < x 
+      x = array[count]
+    end 
+  end 
+  return x 
 end
